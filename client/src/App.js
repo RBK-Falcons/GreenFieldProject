84509dcc06067a54c404e5f308a9ef785e6f605b
IbrahimAtef <bulbul.20001@gmail.com>
2020-07-12 19:20:38 +0300

    version 1

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,26 @@
-import React, { Fragment } from 'react';
+import React from 'react';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
-const App = () => (
-  <Fragment>
-    <h1>App</h1>
-  </Fragment>
-);
+import Login from './components/auth/login';
+import Signup from './components/auth/signup';
+import Teacher from './components/teacherCom/teacher';
+import Landing from './components/landing/landing';
+
+function App() {
+  return (
+    <Router>
+      <div className='App'>
+        <Switch>
+          <Route exact path='/' component={Landing} />
+          <Route path='/sign-in' component={Login} />
+          <Route path='/sign-up' component={Signup} />
+          <Route path='/teacher' component={Teacher} />
+        </Switch>
+      </div>
+    </Router>
+  );
+}
 
 export default App;
