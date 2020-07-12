30c9d5ead85371f0dd14641b04609baa219055a4
dolphen05 <34986941+dolphen05@users.noreply.github.com>
2020-07-12 23:46:41 +0300

    teacherv1

--- client/src/components/teacherCom/teacher.js
+++ client/src/components/teacherCom/teacher.js
@@ -1,33 +1,64 @@
-import React from 'react';
-import $ from 'jquery';
-import img from '../../img/avatar_profile.jpg';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faTachometerAlt } from '@fortawesome/free-solid-svg-icons';
-import { faSpellCheck } from '@fortawesome/free-solid-svg-icons';
-import { faFilm } from '@fortawesome/free-solid-svg-icons';
-import { faPencilAlt } from '@fortawesome/free-solid-svg-icons';
-import { faUniversity } from '@fortawesome/free-solid-svg-icons';
+import React from "react";
+import $ from "jquery";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTachometerAlt } from "@fortawesome/free-solid-svg-icons";
+import { faSpellCheck } from "@fortawesome/free-solid-svg-icons";
+import { faFilm } from "@fortawesome/free-solid-svg-icons";
+import { faPencilAlt } from "@fortawesome/free-solid-svg-icons";
+import { faUniversity } from "@fortawesome/free-solid-svg-icons";
+import { faEye } from "@fortawesome/free-solid-svg-icons";
+import { faEdit } from "@fortawesome/free-solid-svg-icons";
+import { faPlusCircle } from "@fortawesome/free-solid-svg-icons";
+import axios from 'axios';
 
 class Teacher extends React.Component {
-  state = {};
+  state = {
+    title: '',
+    type: '',
+    videoUrl: '',
+    description: ''
+  };
+
+  // this function to handle Data From add videos Form
+  handleChange(e) {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
+
+  // this function to send data to serve to save it in database
+  handleSubmit(e) {
+    e.preventDefault();
+    const { title, type, videoUrl, description } = this.state;
+    axios
+      .post('/api/users/createUser', {
+        title, type, videoUrl, description
+      })
+      .then(res => {
+        console.log(res.data);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
 
   render() {
     return (
-      <div className='teacher'>
+      <div className="teacher">
         {/* Start Header */}
 
-        <div class='header'>
-          <div class='overlay'>
-            <div class='container table'>
-              <div class='navbar'>
+        <div className="header">
+          <div className="overlay">
+            <div className="container table">
+              <div className="navbar">
                 <span>
-                  <span class='main-color wl'>Edu</span>Me
+                  <span className="main-color wl">Edu</span>Me
                 </span>
               </div>
-              <div class='table-row'>
-                <div class='intro text-center'>
-                  <h1 class='upper'>
-                    Welcome <span class='main-color wl'>Ibrahim</span>
+              <div className="table-row">
+                <div className="intro text-center">
+                  <h1 className="upper">
+                    Welcome <span className="main-color wl">Ibrahim</span>
                   </h1>
                 </div>
               </div>
@@ -42,9 +73,9 @@ class Teacher extends React.Component {
 
         {/* Start Our Features */}
 
-        <div class='features'>
-          <div class='container'>
-            <div data-feat='#courses' class='box'>
+        <div className="features">
+          <div className="container">
+            <div data-feat="#courses" className="box">
               <FontAwesomeIcon icon={faFilm} />
               <h3>Courses</h3>
               <p>
@@ -52,7 +83,7 @@ class Teacher extends React.Component {
                 qui officia deserunt mollit anim id est.
               </p>
             </div>
-            <div data-feat='#questions' class='box'>
+            <div data-feat="#questions" className="box">
               <FontAwesomeIcon icon={faPencilAlt} />
               <h3>Questions</h3>
               <p>
@@ -60,7 +91,7 @@ class Teacher extends React.Component {
                 qui officia deserunt mollit anim id est.
               </p>
             </div>
-            <div data-feat='#students' class='box'>
+            <div data-feat="#students" className="box">
               <FontAwesomeIcon icon={faUniversity} />
               <h3>Our Students</h3>
               <p>
@@ -71,26 +102,103 @@ class Teacher extends React.Component {
           </div>
         </div>
 
-        <div className='container'>
-          <div id='courses' className='feat active'>
-            <h1>feature one</h1>
+
+
+        <div className="container">
+          <div id="courses" className="feat text-center active">
+            <h4>Courses</h4>
+            <p className="h1">
+              In this section you can edit, delete or add videos
+            </p>
+            <div className="course-tabs">
+              <div className="sections">
+                <div className="vid" data-section="#add">
+                  <FontAwesomeIcon icon={faPlusCircle} /> Add Video
+                </div>
+                <div className="vid" data-section="#view">
+                  <FontAwesomeIcon icon={faEye} /> View Video
+                </div>
+                <div className="vid" data-section="#edit">
+                  <FontAwesomeIcon icon={faEdit} /> Edit Video
+                </div>
+              </div>
+              <section>
+                <div id="add">
+                  <form onSubmit={this.handleSubmit.bind(this)}>
+                    <div className="form-group" >
+                      <label>Enter Video Title</label>
+                      <input
+                        type="text"
+                        name="title"
+                        className="form-control"
+                        placeholder="Enter Video Title"
+                        value={this.state.title}
+                        onChange={this.handleChange.bind(this)}
+                      />
+                    </div>
+                    <div className="form-group">
+                      <label>Enter Video Type</label>
+                      <input
+                        type="text"
+                        name="type"
+                        className="form-control"
+                        placeholder="Enter Video Type"
+                        value={this.state.type}
+                        onChange={this.handleChange.bind(this)} />
+                    </div>
+                    <div className="form-group">
+                      <label>Enter Video URL</label>
+                      <input
+                        type="text"
+                        name="videoUrl"
+                        className="form-control"
+                        placeholder="Enter Video URL"
+                        value={this.state.videoUrl}
+                        onChange={this.handleChange.bind(this)}
+                      />
+                    </div>
+                    <div className="form-group">
+                      <label>Enter Video Description</label>
+                      <textarea
+                        name="description"
+                        placeholder="Enter Video Description"
+                        className="form-control"
+                        value={this.state.description}
+                        onChange={this.handleChange.bind(this)}></textarea>
+                    </div>
+                    <button type="submit" className="btn btn-primary">
+                      Add Video
+                    </button>
+                  </form>
+                </div>
+                <div id="view" >
+                  <h2>view Video</h2>
+                </div>
+                <div id="edit" >
+                  <h2>edit Video</h2>
+                </div>
+              </section>
+            </div>
           </div>
-          <div id='questions' className='feat'>
+          <div id="questions" className="feat">
             <h1>feature two</h1>
           </div>
-          <div id='students' className='feat'>
+          <div id="students" className="feat">
             <h1>feature three</h1>
           </div>
         </div>
 
+        {/* this div for clear float */}
+        <div className="clearFix"></div>
+
         {/* End Our Features */}
 
         {/* Start Footer */}
-        <footer className='text-center'>
+        <footer className="text-center">
           Copyright &copy; <span>EduMe</span> 2020
         </footer>
         {/* Start Footer */}
-      </div>
+      </div >
     );
   }
 }
@@ -100,11 +208,20 @@ export default Teacher;
 $(document).ready(function () {
   // Change Header Height
 
-  $('.header').height($(window).height());
+  $(".header").height($(window).height());
 
-  // Features tab
-  $('.features .box').click(function () {
-    var feat = $(this).data('feat');
-    $(feat).addClass('active').siblings().removeClass('active');
+  // Main Features tab
+  $(".features .box").click(function () {
+    var feat = $(this).data("feat");
+    $(feat).show().siblings().hide();
+  });
+
+  // Section Of Features Tab
+
+  $('.feat .course-tabs .sections .vid').click(function () {
+    var section = $(this).data("section");
+    $(section).parent().show()
+    $(section).show().siblings().hide();
+    console.log(section);
   });
 });
