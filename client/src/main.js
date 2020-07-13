e35b06fb9b4e1cefa03306df19e4f6d0c9ff15a7
IbrahimAtef <bulbul.20001@gmail.com>
2020-07-13 15:20:54 +0300

    back course v1

--- client/src/main.js
+++ client/src/main.js
@@ -1,75 +1,67 @@
-// import jQuery 
+// import jQuery
 import $ from 'jquery';
 
 $(document).ready(function () {
-    // Change Header Height
+  // Change Header Height
 
-    $(".header").height($(window).height());
+  $('.header').height($(window).height());
 
-    // Main Features tab
-    $(".features .box").click(function () {
-        var feat = $(this).data("feat");
-        $(feat).slideDown().siblings().slideUp();
-    });
+  // Main Features tab
+  $('.features .box').click(function () {
+    var feat = $(this).data('feat');
+    $(feat).slideDown().siblings().slideUp();
+  });
 
-    // Section Of Features Tab
+  // Section Of Features Tab
 
-    $('.teacher .feat .course-tabs .sections .vid').click(function () {
-        var section = $(this).data("section");
-        $(section).parent().slideDown()
-        $(section).slideDown().siblings().slideUp();
-        console.log(section);
-    });
+  $('.teacher .feat .course-tabs .sections .vid').click(function () {
+    var section = $(this).data('section');
+    $(section).parent().slideDown();
+    $(section).slideDown().siblings().slideUp();
+    console.log(section);
+  });
 
+  // Pop Up Videos In Student Page (Courses)
 
+  $('.video-popup').on('click', function (e) {
+    e.preventDefault();
+    var videoUrl = $(this).attr('data-media');
+    var popupIframe = $('#popup-frame');
 
+    popupIframe.attr('src', videoUrl);
+    $('.popup').addClass('show-popup');
+  });
 
-    // Pop Up Videos In Student Page (Courses)
+  $('.popup').on('click', function (e) {
+    e.preventDefault();
+    e.stopPropagation();
 
-    $('.video-popup').on('click', function (e) {
-        e.preventDefault();
-        var videoUrl = $(this).attr('data-media');
-        var popupIframe = $('#popup-frame');
+    $('#popup-frame').attr('src', '');
+    $('.popup').removeClass('show-popup');
+  });
 
-        popupIframe.attr("src", videoUrl);
-        $(".popup").addClass("show-popup");
-    })
+  $('.popup > iframe').on('click', function (e) {
+    e.stopPropagation();
+  });
 
-    $(".popup").on("click", function (e) {
-        e.preventDefault();
-        e.stopPropagation();
+  // Trigger MixitUp
+  // $('.feat .course-videos .sections div').click(function () {
+  //     var type = $(this).data('filter')
+  //     console.log(type)
+  //     if (type == 'all') {
+  //         $('.feat .course-videos section ').show()
+  //     } else if (type == 'technical-skills') {
+  //         $('.' + type).show().siblings().hide()
+  //     } else if (type == 'non-technical') {
+  //         $('.' + type).show().siblings().hide()
+  //     } else if (type == 'other') {
+  //         $('.' + type).show().siblings().hide()
+  //     }
+  // })
 
-        $('#popup-frame').attr('src', '');
-        $(".popup").removeClass("show-popup");
-    });
-
-    $(".popup > iframe").on("click", function (e) {
-        e.stopPropagation();
-    });
-
-
-    // Trigger MixitUp
-    // $('.feat .course-videos .sections div').click(function () {
-    //     var type = $(this).data('filter')
-    //     console.log(type)
-    //     if (type == 'all') {
-    //         $('.feat .course-videos section ').show()
-    //     } else if (type == 'technical-skills') {
-    //         $('.' + type).show().siblings().hide()
-    //     } else if (type == 'non-technical') {
-    //         $('.' + type).show().siblings().hide()
-    //     } else if (type == 'other') {
-    //         $('.' + type).show().siblings().hide()
-    //     }
-    // })
-
-    // var mixer = mixitup('.feat .course-videos section', {
-    //     animation: {
-    //         effectsOut: 'fade translateX(-100%)'
-    //     }
-    // })
-
-
-
-
+  // var mixer = mixitup('.feat .course-videos section', {
+  //     animation: {
+  //         effectsOut: 'fade translateX(-100%)'
+  //     }
+  // })
 });
