26ff97f58ff49ec9f0805cf597c6ae448dd8d57a
IbrahimAtef <bulbul.20001@gmail.com>
2020-07-18 14:13:33 +0300

    final changes 1.4

--- client/src/components/loadingPage/loading.js
+++ client/src/components/loadingPage/loading.js
@@ -4,8 +4,8 @@ class Loading extends React.Component {
   componentDidMount() {
     // $(window).on('load', function () {
     setTimeout(() => {
-      $('.loading').fadeOut(1000);
-    }, 2000);
+      $('.loading').fadeOut(3000);
+    }, 3000);
     // });
   }
   render() {
